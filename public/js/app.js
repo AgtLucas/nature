50f7299e8e01f9b37f2b27df77aea718a06715ba
Lucas Churchill <lucas@agtlucas.com>
2014-04-14 22:57:18 -0300

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -34,7 +34,31 @@
           self.renderTemplate(data);
         });
       });
-    }
+    },
+
+    /**
+     * Render the images on the page and check for layout resize
+     */
+    renderTemplate: function () {
+      var lastAnimate,
+        lastSrc,
+        nextSrc,
+        last,
+        current = data.data[0].images.standard_resolution.url,
+        w = $(document).width(),
+        query = data,
+        source = $('.most-recent-tpl').html(),
+        compiledTemplate = Handlebars.complete(source),
+        result = compiledTemplate(query),
+        imgWrap = $('.img-content');
+
+      imgWrap.prepend(default);
+
+      last = $('.img-content a:first-child');
+      lastSrc = $('.img-content a:first-child').find('img').attr('src');
+      nextSrc = $('.img-content a:nth-child(2)').find('img').attr('src');
+
+    },
 
   }
 
